9be3867b358cf447da6a55c4fb105af4c5263500
Onyeka <onyeka@example.com>
2026-04-03 20:24:05 -0500

    Add service worker for auto-update from home screen

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_VERSION = 'fasttrack-v9';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE_VERSION).then(cache => cache.put(e.request, clone));
+      return response;
+    }).catch(() => caches.match(e.request))
+  );
+});
